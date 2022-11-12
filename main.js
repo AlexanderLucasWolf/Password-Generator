3b0946c500dfafa9f37dc6c749f0d9ff6d6ffb95
Alexander L. Wolf <alucaswolf@gmail.com>
2022-11-12 12:44:05 +0100

    Copy function added

--- main.js
+++ main.js
@@ -173,7 +173,24 @@ function calculateStrength() {
 
 //Copy text
 
-function copy() {
+function copyText(htmlElement) {
+  if (!htmlElement) {
+    return;
+  }
+
+  let elementText = htmlElement.innerText;
+  let inputElement = document.createElement("input");
+  inputElement.setAttribute("value", elementText);
+  document.body.appendChild(inputElement);
+
+  inputElement.select();
+  document.execCommand("copy");
+  inputElement.parentNode.removeChild(inputElement);
+}
+
+document.querySelector("#copy-icon").onclick =
+function() {
+  copyText(document.querySelector("#final-password"));
 }
 
 
